34d3f837dd4bed9eb49480b3c04f906bd89eb557
Crowdbotics <team@crowdbotics.com>
2021-10-14 20:48:10 +0000

    Initial commit.

---
+++ backend/odd_cake_31431/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for odd_cake_31431 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'odd_cake_31431.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "odd_cake_31431",
+  url: "https://odd_cake_31431.botics.co",
+  api: "https://odd_cake_31431.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
